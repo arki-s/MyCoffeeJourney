8779310f1b19ff0ee670086dfadc79db2f61e1ae
exec zsh <wind10cloud@gmail.com>
2024-03-06 19:21:45 +0900

    added expo sqlite

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
+
+import * as FileSystem from 'expo-file-system';
+import * as SQLite from 'expo-sqlite';
+import { Asset } from 'expo-asset';
 
 
 export default function App() {
@@ -10,6 +14,17 @@ export default function App() {
     'Ojuju-M': require('./assets/fonts/Ojuju-Medium.ttf'),
     'Ojuju-L': require('./assets/fonts/Ojuju-Light.ttf'),
   });
+
+  async function openDatabase(pathToDatabaseFile: string): Promise<SQLite.WebSQLDatabase> {
+    if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+      await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+    }
+    await FileSystem.downloadAsync(
+      Asset.fromModule(require('./')).uri,
+      FileSystem.documentDirectory + 'SQLite/MyCoffeeJourney.db'
+    );
+    return SQLite.openDatabase('MyCoffeeJourney.db');
+  }
 
   return (
     <View style={styles.container}>
