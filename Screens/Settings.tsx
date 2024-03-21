a1dea4224804a30bf409ca3b429b27ada99147f5
exec zsh <wind10cloud@gmail.com>
2024-03-21 18:43:25 +0900

    added reading brands and beans feature

--- Screens/Settings.tsx
+++ Screens/Settings.tsx
@@ -1,13 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { globalStyles } from '../Styles/globalStyles'
 import Header from './Header'
+import { CoffeeBean, CoffeeBrand } from '../types';
+import { useSQLiteContext } from 'expo-sqlite/next';
 
 export default function Settings() {
+  const [brands, setBrands] = useState<CoffeeBrand[]>([]);
+  const [beans, setBeans] = useState<CoffeeBean[]>([]);
+
+  const db = useSQLiteContext();
+
+  useEffect(() => {
+    db.withExclusiveTransactionAsync(async () => {
+      await getData();
+    })
+  }, [db])
+
+  async function getData() {
+    await db.getAllAsync<CoffeeBrand>(`
+    SELECT * FROM coffeeBrand;`).then((rsp) => {
+      console.log("rsp", rsp);
+      setBrands(rsp);
+    }).catch((error) => {
+      console.log("reading coffee brand error!");
+      console.log(error.message);
+    });
+
+    await db.getAllAsync<CoffeeBean>(`
+    SELECT * FROM coffeeBean;`).then((rsp) => {
+      console.log("rsp", rsp);
+      setBeans(rsp);
+    }).catch((error) => {
+      console.log("reading coffee bean error!");
+      console.log(error.message);
+    });
+  }
+
+  const brandList = brands.map((br) => {
+    return (
+      <Text>{br.name}</Text>
+    )
+  });
+
+  const beanList = beans.map((be) => {
+    return (
+      <Text>{be.name}</Text>
+    )
+  });
+
+
   return (
     <View style={globalStyles.container}>
       <Header title={'設定'} />
-      <Text>Settings</Text>
+      <TouchableOpacity style={{ backgroundColor: "red", padding: 20 }}>
+        <Text>Add New Coffee Brand</Text>
+      </TouchableOpacity>
+      {brandList}
+      <TouchableOpacity style={{ backgroundColor: "pink", padding: 20 }}>
+        <Text>Add New Coffee Bean</Text>
+      </TouchableOpacity>
+      {beanList}
     </View>
   )
 }
