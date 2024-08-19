b880e0be2f6f0c42ae462521268f98f8b9841875
exec zsh <wind10cloud@gmail.com>
2024-08-19 13:48:27 +0900

    added total coffee, bean and brand count to the analytics page

--- Screens/Analytics.tsx
+++ Screens/Analytics.tsx
@@ -11,6 +11,9 @@ export default function Analytics({ navigation }: { navigation: NativeStackNavig
   const [totalCount, setTotalCount] = useState(0);
   const [averageGram, setAverageGram] = useState(0);
   const [totalGram, setTotalGram] = useState(0);
+  const [totalCountCoffee, setTotalCountCoffee] = useState(0);
+  const [totalCountBean, setTotalCountBean] = useState(0);
+  const [totalCountBrand, setTotalCountBrand] = useState(0);
 
   const db = useSQLiteContext();
 
@@ -27,7 +30,7 @@ export default function Analytics({ navigation }: { navigation: NativeStackNavig
       FROM record
       WHERE endDate IS NOT NULL;
       `).then((rsp) => {
-      console.log("count", rsp[0]);
+      // console.log("count", rsp[0]);
       const count: any = rsp[0];
       setTotalCount(count.count);
     }
@@ -41,11 +44,47 @@ export default function Analytics({ navigation }: { navigation: NativeStackNavig
       FROM record
       WHERE endDate IS NOT NULL;
       `).then((rsp) => {
-      console.log("total gram", rsp[0]);
+      // console.log("total gram", rsp[0]);
       const total: any = rsp[0];
       setTotalGram(total.total);
     }).catch((error) => {
       console.log("total gram error!");
+      console.log(error.message);
+    })
+
+    await db.getAllAsync(`
+      SELECT COUNT(*) AS count
+      FROM coffee;
+      `).then((rsp) => {
+      // console.log("total count coffee", rsp[0]);
+      const totalCoffee: any = rsp[0];
+      setTotalCountCoffee(totalCoffee.count);
+    }).catch((error) => {
+      console.log("total coffee Count error!");
+      console.log(error.message);
+    })
+
+    await db.getAllAsync(`
+      SELECT COUNT(*) AS count
+      FROM coffeeBean;
+      `).then((rsp) => {
+      // console.log("total count bean", rsp[0]);
+      const totalBean: any = rsp[0];
+      setTotalCountBean(totalBean.count);
+    }).catch((error) => {
+      console.log("total bean Count error!");
+      console.log(error.message);
+    })
+
+    await db.getAllAsync(`
+      SELECT COUNT(*) AS count
+      FROM coffeeBrand;
+      `).then((rsp) => {
+      // console.log("total count brand", rsp[0]);
+      const totalBrand: any = rsp[0];
+      setTotalCountBrand(totalBrand.count);
+    }).catch((error) => {
+      console.log("total brand Count error!");
       console.log(error.message);
     })
 
@@ -64,16 +103,20 @@ export default function Analytics({ navigation }: { navigation: NativeStackNavig
     // })
   }
 
+  // 飲んだ回数、総グラム、図鑑登録コーヒー数、登録コーヒー豆数、登録ブランド数、飲んだ回数が一番多いコーヒー
 
 
   return (
     <View style={globalStyles.container}>
       <ImageBackground source={require('../assets/texture.jpg')} style={globalStyles.imgBackground}>
         <Header title={'分析'} />
+        <Text>図鑑登録コーヒー数: {totalCountCoffee}</Text>
+        <Text>登録コーヒー豆数: {totalCountBean}</Text>
+        <Text>登録コーヒーブランド数: {totalCountBrand}</Text>
         <Text>コーヒーを飲んだ回数: {totalCount}</Text>
         {/* <Text>一日のコーヒー平均グラム: {averageGram}g</Text> */}
         <Text>飲んだコーヒーの総グラム: {totalGram}g</Text>
-        <Text>各ランキング</Text>
+        <Text>飲んだ回数が多いコーヒー（上位3種類）</Text>
       </ImageBackground>
     </View>
   )
