429346c15962d9574a99615be9cd7620acb57134
exec zsh <wind10cloud@gmail.com>
2024-08-19 14:34:24 +0900

    added styles to the analytics page

--- Screens/Analytics.tsx
+++ Screens/Analytics.tsx
@@ -30,103 +30,122 @@ export default function Analytics({ navigation }: { navigation: NativeStackNavig
     })
   }
 
-  useEffect(() => {
-    db.withExclusiveTransactionAsync(async () => {
-      await getData();
-    })
-  }, [db])
+  // useEffect(() => {
+  //   db.withExclusiveTransactionAsync(async () => {
+  //     await getData();
+  //   })
+  // }, [db])
 
-  async function getData() {
+  // async function getData() {
 
-    // await db.getAllAsync(`
-    //   SELECT COUNT(*) AS count
-    //   FROM record
-    //   WHERE endDate IS NOT NULL;
-    //   `).then((rsp) => {
-    //   // console.log("count", rsp[0]);
-    //   const count: any = rsp[0];
-    //   setTotalCount(count.count);
-    // }
-    // ).catch((error) => {
-    //   console.log("count error!");
-    //   console.log(error.message);
-    // })
+  // await db.getAllAsync(`
+  //   SELECT COUNT(*) AS count
+  //   FROM record
+  //   WHERE endDate IS NOT NULL;
+  //   `).then((rsp) => {
+  //   // console.log("count", rsp[0]);
+  //   const count: any = rsp[0];
+  //   setTotalCount(count.count);
+  // }
+  // ).catch((error) => {
+  //   console.log("count error!");
+  //   console.log(error.message);
+  // })
 
-    // await db.getAllAsync(`
-    //   SELECT SUM(gram) AS total
-    //   FROM record
-    //   WHERE endDate IS NOT NULL;
-    //   `).then((rsp) => {
-    //   // console.log("total gram", rsp[0]);
-    //   const total: any = rsp[0];
-    //   setTotalGram(total.total);
-    // }).catch((error) => {
-    //   console.log("total gram error!");
-    //   console.log(error.message);
-    // })
+  // await db.getAllAsync(`
+  //   SELECT SUM(gram) AS total
+  //   FROM record
+  //   WHERE endDate IS NOT NULL;
+  //   `).then((rsp) => {
+  //   // console.log("total gram", rsp[0]);
+  //   const total: any = rsp[0];
+  //   setTotalGram(total.total);
+  // }).catch((error) => {
+  //   console.log("total gram error!");
+  //   console.log(error.message);
+  // })
 
-    // await db.getAllAsync(`
-    //   SELECT COUNT(*) AS count
-    //   FROM coffee;
-    //   `).then((rsp) => {
-    //   // console.log("total count coffee", rsp[0]);
-    //   const totalCoffee: any = rsp[0];
-    //   setTotalCountCoffee(totalCoffee.count);
-    // }).catch((error) => {
-    //   console.log("total coffee Count error!");
-    //   console.log(error.message);
-    // })
+  // await db.getAllAsync(`
+  //   SELECT COUNT(*) AS count
+  //   FROM coffee;
+  //   `).then((rsp) => {
+  //   // console.log("total count coffee", rsp[0]);
+  //   const totalCoffee: any = rsp[0];
+  //   setTotalCountCoffee(totalCoffee.count);
+  // }).catch((error) => {
+  //   console.log("total coffee Count error!");
+  //   console.log(error.message);
+  // })
 
-    // await db.getAllAsync(`
-    //   SELECT COUNT(*) AS count
-    //   FROM coffeeBean;
-    //   `).then((rsp) => {
-    //   // console.log("total count bean", rsp[0]);
-    //   const totalBean: any = rsp[0];
-    //   setTotalCountBean(totalBean.count);
-    // }).catch((error) => {
-    //   console.log("total bean Count error!");
-    //   console.log(error.message);
-    // })
+  // await db.getAllAsync(`
+  //   SELECT COUNT(*) AS count
+  //   FROM coffeeBean;
+  //   `).then((rsp) => {
+  //   // console.log("total count bean", rsp[0]);
+  //   const totalBean: any = rsp[0];
+  //   setTotalCountBean(totalBean.count);
+  // }).catch((error) => {
+  //   console.log("total bean Count error!");
+  //   console.log(error.message);
+  // })
 
-    // await db.getAllAsync(`
-    //   SELECT COUNT(*) AS count
-    //   FROM coffeeBrand;
-    //   `).then((rsp) => {
-    //   // console.log("total count brand", rsp[0]);
-    //   const totalBrand: any = rsp[0];
-    //   setTotalCountBrand(totalBrand.count);
-    // }).catch((error) => {
-    //   console.log("total brand Count error!");
-    //   console.log(error.message);
-    // })
+  // await db.getAllAsync(`
+  //   SELECT COUNT(*) AS count
+  //   FROM coffeeBrand;
+  //   `).then((rsp) => {
+  //   // console.log("total count brand", rsp[0]);
+  //   const totalBrand: any = rsp[0];
+  //   setTotalCountBrand(totalBrand.count);
+  // }).catch((error) => {
+  //   console.log("total brand Count error!");
+  //   console.log(error.message);
+  // })
 
-    // await db.getAllAsync(`
-    //   SELECT SUM(gram / julianday(endDate) - julianday(startDate)) AS average
-    //   FROM record
-    //   WHERE endDate IS NOT NULL
-    //   AND startDate <> endDate;
-    //   `).then((rsp) => {
-    //   console.log("average gram", rsp);
-    //   const average: any = rsp[0];
-    //   setAverageGram(average.average);
-    // }).catch((error) => {
-    //   console.log("average gram error!");
-    //   console.log(error.message);
-    // })
-  }
+  // await db.getAllAsync(`
+  //   SELECT SUM(gram / julianday(endDate) - julianday(startDate)) AS average
+  //   FROM record
+  //   WHERE endDate IS NOT NULL
+  //   AND startDate <> endDate;
+  //   `).then((rsp) => {
+  //   console.log("average gram", rsp);
+  //   const average: any = rsp[0];
+  //   setAverageGram(average.average);
+  // }).catch((error) => {
+  //   console.log("average gram error!");
+  //   console.log(error.message);
+  // })
+  // }
 
 
   return (
     <View style={globalStyles.container}>
       <ImageBackground source={require('../assets/texture.jpg')} style={globalStyles.imgBackground}>
         <Header title={'分析'} />
-        <Text>図鑑登録コーヒー数: {coffees ? coffees.length : 0}</Text>
-        <Text>登録コーヒー豆数: {beans ? beans.length : 0}</Text>
-        <Text>登録コーヒーブランド数: {brands ? brands.length : 0}</Text>
-        <Text>コーヒーを飲んだ回数: {recordCount}</Text>
-        <Text>飲んだコーヒーの総グラム: {totalGram}g</Text>
-        {/* <Text>飲んだ回数が多いコーヒー（上位3種類）</Text> */}
+        <View style={analyticsStyles.statsContainer}>
+          <View style={analyticsStyles.statsSubContainer}>
+            <Text style={analyticsStyles.statsText}>図鑑コーヒー数: </Text>
+            <Text style={analyticsStyles.statsText}>{coffees ? coffees.length : 0}</Text>
+          </View>
+          <View style={analyticsStyles.statsSubContainer}>
+            <Text style={analyticsStyles.statsText}>コーヒー豆数: </Text>
+            <Text style={analyticsStyles.statsText}>{beans ? beans.length : 0}</Text>
+          </View>
+          <View style={analyticsStyles.statsSubContainer}>
+            <Text style={analyticsStyles.statsText}>コーヒーブランド数: </Text>
+            <Text style={analyticsStyles.statsText}>{brands ? brands.length : 0}</Text>
+          </View>
+          <View style={analyticsStyles.statsSubContainer}>
+            <Text style={analyticsStyles.statsText}>コーヒーを飲んだ回数: </Text>
+            <Text style={analyticsStyles.statsText}>{recordCount}</Text>
+          </View>
+
+          <View style={analyticsStyles.statsGramContainer}>
+            <Text style={analyticsStyles.statsText}>飲んだコーヒー総グラム: </Text>
+            <Text style={[analyticsStyles.statsText, { textAlign: 'right' }]}>{totalGram.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}g</Text>
+          </View>
+
+          {/* <Text>飲んだ回数が多いコーヒー（上位3種類）</Text> */}
+        </View>
       </ImageBackground>
     </View>
   )
--- Styles/analyticsStyles.ts
+++ Styles/analyticsStyles.ts
@@ -2,5 +2,36 @@ import { StyleSheet } from 'react-native';
 import Colors from './Colors';
 
 export const analyticsStyles = StyleSheet.create({
+  statsContainer : {
+    width:'90%',
+    // paddingVertical:10,
+    paddingHorizontal:14,
+    borderRadius:10,
+    backgroundColor:Colors.PRIMARY,
+    borderWidth:3,
+    borderColor:Colors.SECONDARY_LIGHT,
+    // alignItems:'center',
+    // justifyContent:'center',
+    marginTop:15,
+    alignSelf:'center',
+  },
+
+  statsSubContainer:{
+    paddingVertical:10,
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-between',
+  },
+
+  statsGramContainer:{
+    paddingVertical:10
+  },
+
+  statsText:{
+    fontFamily:'yusei',
+    fontSize:22,
+    color:Colors.SECONDARY_LIGHT,
+  },
+
 
 })
